3510e4ff217e0463762a3acc1d1ce360b16ed560
Cristina Natea <cristina.natea4@gmail.com>
2025-07-08 15:03:30 +0300

    Adaugare dictionar

--- decorator.py
+++ decorator.py
@@ -45,9 +45,11 @@ def decorator(cnp):
         print("Data nu e valida")
         return
 
+    numar_judet = int(cnp[7:9])
+    dictionar ={ 1 :'Alba', 2 :'Arad', 3 :'Arges', 4 :'Bacau', 5 :'Bihor', 6 :'Bistrita-Nasaud', 7 :'Botosani', 8 :'Brasov', 9 :'Braila',  10 :'Buzau', 11 :'Caras-Severin', 12 :'Cluj', 13 :'Constanta',14:'Covasna',15:'Dambovita',16:'Dolj',17:'Galati',18:'Dolj',19:'Harghita',20:'Hunedoara',21:'Ialomita',22:'iasi',23:'Ilfov',24:'Maramures',25:'Mehedinti',26:'Mures',27:'Neamt',28:'Olt',29:'Prahova',30:'Satu mare',31:'Salaj',32:'Sibiu',33:'Suceava',34:'Teleorman',35:'Timis',36:'Tulcea',37:'Vaslui',38:'Valcea',39:'Vrancea',40:'Bucuresti',41:'Bucuresti s1',42:'Bucuresti s2',43:'Bucuresti s3',44:'Bucuresti s4',45:'BUcuresti s5',46:'BUcuresti s6',51:'Calarasi',52:'Giurgiu'}
 
-    judet = int(cnp[7:9])
-    print(f"Judetul in care s-a nascut persoana este: {judet}")
+    judete = dictionar.get(numar_judet)
+    print(f"Judetul in care s-a nascut persoana este: {judete}")
     cod_secvential = int (cnp[9:12])
     print(f"Codul secvential al persoanei este : {cod_secvential}")
     cifra_control = int (cnp[12])
@@ -64,8 +66,6 @@ def decorator(cnp):
         print(f"Cifra de control este: {cifra_finala}")
     else:
         print("Calculul nu e valid")
-
-
 
 result = decorator(5230205325021)
 
